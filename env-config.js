8e9f26b2039e11f61d50e8f2f8619001147d35bb
taeeun kim <kimtaeeeny@gmail.com>
2020-04-23 06:37:17 +0900

    github page

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/small-magic-project-deployment' : '',
+  'process.env.BACKEND_URL': prod ? '/Next-gh-page-example' : '',
 }
